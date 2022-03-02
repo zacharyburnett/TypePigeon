cfea9d809bac85d09fc04518f2ad3efda75eb102
Zach <zachary.r.burnett@gmail.com>
2022-03-02 09:54:54 -0500

    offload versioning and installation into `setup.cfg` and `pyproject.toml` (#1)
    * change to `pyproject.toml`
    
    * change to `pyproject.toml`
    
    * simplify setup
    
    * mark test
    
    * change to `pyproject.toml`
    
    * change to `pyproject.toml`
    
    * update pyproject.toml
    
    * revert
    
    * workflows
    
    * cache formatting workflow
    
    * remove formatting workflow

--- setup.py
+++ setup.py
@@ -1,30 +1,12 @@
 import warnings
 
-from setuptools import config, find_packages, setup
+from dunamai import Version
+from setuptools import find_packages, setup
 
 try:
-    from dunamai import Version
-except (ModuleNotFoundError, ImportError):
-    raise ModuleNotFoundError('pakcage "dunamai" not found')
-
-try:
-    version = Version.from_any_vcs().serialize()
+    __version__ = Version.from_any_vcs().serialize()
 except RuntimeError as error:
     warnings.warn(f'{error.__class__.__name__} - {error}')
-    version = '0.0.0'
+    __version__ = '0.0.0'
 
-metadata = config.read_configuration('setup.cfg')['metadata']
-
-setup(
-    **metadata,
-    version=version,
-    packages=find_packages(),
-    python_requires='>=3.6',
-    setup_requires=['dunamai', 'setuptools>=41.2'],
-    install_requires=['pyproj', 'python-dateutil', 'shapely'],
-    extras_require={
-        'testing': ['pytest', 'pytest-cov', 'pytest-xdist', 'wget'],
-        'development': ['flake8', 'isort', 'oitnb'],
-        'documentation': ['m2r2', 'sphinx', 'sphinx-rtd-theme'],
-    },
-)
+setup(version=__version__, packages=find_packages(exclude=('tests',)))
